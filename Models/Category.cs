d464b60f2d2642293d56346aad3f1e38d45e0e0b
Abdelwahab <hopakamal25@gmail.com>
2022-10-06 05:52:54 +0200

    hopa first commit

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Maqeem.Models
 {
     public class Category
     {
+        [Key]
         public int CategoryID { get; set; }
+
+        [Required(ErrorMessage="Please enter your name"),MaxLength(50)]
         public string CategoryName { get; set; }
 
+        [ForeignKey("CategoryGroupID")]
         public IEnumerable<CategoryGroup> CategoryGroups { get; set; }
     }
 }
