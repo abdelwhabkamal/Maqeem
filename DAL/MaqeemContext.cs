5a8e847f45321790dac9ee659f61f6922ff71dcb
Abdelwahab <hopakamal25@gmail.com>
2022-10-06 21:30:41 +0200

    hopa commit

---
+++ DAL/MaqeemContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Maqeem.Models;
+
+namespace Maqeem.DAL
+{
+    public class MaqeemContext:DbContext
+    {
+        public MaqeemContext() : base("MaqeemContext")
+        {
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Buyer> Buyers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<CategoryGroup> CategoryGroups { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Deal> Deals { get; set; }
+        public DbSet<DealType> DealTypes { get; set; }
+        public DbSet<Property> Properties { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
